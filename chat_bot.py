de4ee0ec57219dc8b169798fd624dfffa5502e78
Ming <ming.lei@shopcider.com>
2021-01-29 13:23:05 +0800

    set constraints in url

--- chat_bot.py
+++ chat_bot.py
@@ -1,4 +1,5 @@
 import FEISHU_CHAT_BOT
+import update_status
 import time
 import schedule
 import test
@@ -17,9 +18,20 @@ def logistics_update_failed_id_job():
             for val in res[keys]:
                 text += '\t' + str(val) + '\n'
     if text == "":
-        text += '无更新失败信息'
-    bot.send_post(msg = 'text', title='物流更新信息:\n')
+        text += '无物流更新失败信息'
+    bot.send_post(msg = text, title='物流更新信息:\n')
 
+def refund_update_failed_id_job():
+    res = update_status.update_refund_status()
+    msg = ""
+    for keys in res:
+        if res[keys]:
+            msg += keys + '\n'
+            for val in res[keys]:
+                msg += '\t' + str(val) + '\n'
+    if msg == "":
+        msg += '无状态更新失败信息'
+    bot.send_post(msg=msg, title='退货状态更新信息:\n')
 
 if __name__ == '__main__':
     logistics_update_failed_id_job()
